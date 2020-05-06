368734061260191718b08e37f8e1517431714e4c
caijianwei <18367157420@163.com>
2020-05-06 23:04:48 +0800

    测试mysql_until工具类

--- util/mysql_util.py
+++ util/mysql_util.py
@@ -42,3 +42,13 @@ class MysqlUtil:
         except Exception as e:
             self.conn.rollback()
             print(e)
+
+
+if __name__ == '__main__':
+    db = MysqlUtil('127.0.0.1', 3306, 'root', '123456', 'interfacetest')
+    sql1 = 'select * from user'
+    rs1 = db.query(sql1)
+    for rs in rs1:
+        print(rs)
+    # sql2 = 'insert into user values ("小明",17,"男")'
+    # db.execute(sql2)
